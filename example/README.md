04518b4742ff4ae875e4706381b2cb2c60b4dadd
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-10-12 02:03:06 +0200

    Add a tiny example

---
+++ example/README.md
@@ -0,0 +1,9 @@
+# Example
+
+Compile with:
+
+    npm install
+
+Load into a running process:
+
+    frida Twitter -l _agent.js
---
+++ example/agent.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const irc = require('irc');
+
+const server = 'irc.freenode.net';
+const channel = '#frida';
+const nick = 'Inside' + Process.enumerateModulesSync()[0].name;
+
+const client = new irc.Client(server, nick, {
+  channels: [channel],
+});
+client.on('connect', _ => {
+  client.conn.setNoDelay(true);
+});
+client.on('error', error => {
+  console.log('*** ERROR: ' + JSON.stringify(error, null, 2));
+});
+client.on('message' + channel, (from, message) => {
+  const trigger = client.nick + ': ';
+  if (message.indexOf(trigger) === -1)
+    return;
+  const code = message.substr(trigger.length);
+
+  let result;
+  try {
+    const rawResult = (1, eval)(code);
+    global._ = rawResult;
+    result = JSON.stringify(rawResult);
+  } catch (e) {
+    result = 'throw new ' + e.name + '("' + e.message + '")';
+  }
+
+  client.say(channel, `${from}: ${result}`);
+});
