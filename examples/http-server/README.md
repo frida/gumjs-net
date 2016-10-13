f67796e886a0cfd4b01180c150c7b87bdd5d42dd
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-10-13 05:38:15 +0200

    Add an HTTP server example
    Only runs on the V8 runtime for now due to frida-compile/babelify needing
    some tweaks.

---
+++ examples/http-server/README.md
@@ -0,0 +1,13 @@
+# HTTP server example
+
+Compile with:
+
+    npm install
+
+Load into a running process:
+
+    frida Twitter -l _agent.js
+
+Talk to it:
+
+    curl http://127.0.0.1:1337/modules
---
+++ examples/http-server/agent.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const app = require('koa')();
+const router = require('koa-router')();
+
+router
+  .get('/ranges', function *(next) {
+    this.body = Process.enumerateRangesSync({
+      protection: '---',
+      coalesce: true
+    });
+  })
+  .get('/modules', function *(next) {
+    this.body = Process.enumerateModulesSync();
+  })
+  .get('/modules/:name', function *(next) {
+    try {
+      this.body = Process.getModuleByName(this.params.name);
+    } catch (e) {
+      this.status = 404;
+      this.body = e.message;
+    }
+  })
+  .get('/modules/:name/exports', function *(next) {
+    this.body = Module.enumerateExportsSync(this.params.name);
+  })
+  .get('/modules/:name/imports', function *(next) {
+    this.body = Module.enumerateImportsSync(this.params.name);
+  })
+  .get('/objc/classes', function *(next) {
+    if (ObjC.available) {
+      this.body = Object.keys(ObjC.classes);
+    } else {
+      this.status = 404;
+      this.body = 'Objective-C runtime not available in this process';
+    }
+  })
+  .get('/threads', function *(next) {
+    this.body = Process.enumerateThreadsSync();
+  });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(1337);
